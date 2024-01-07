45be8048a8e12f20e22db8c4712c5e626c3c35e9
Nivedita Kaur <niveditakaur5244@gmail.com>
2024-01-07 17:56:36 +0530

    Time: 134 ms (100.00%), Space: 70.5 MB (100.00%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numberOfArithmeticSlices = function (nums) {
+    const n = nums.length;
+    const dp = Array.from(Array(n), () => Array(n).fill(0));
+    const map = new Map();
+
+    for (let i = 0; i < n; i++) {
+        const temp = nums[i];
+        if (!map.has(temp)) {
+            map.set(temp, []);
+        }
+        map.get(temp).push(i);
+    }
+
+    let totalSum = 0;
+    for (let i = 1; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            const a = 2 * nums[i] - nums[j];
+            if (map.has(a)) {
+                for (const k of map.get(a)) {
+                    if (k < i) {
+                        dp[i][j] += dp[k][i] + 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            totalSum += dp[i][j];
+        }
+    }
+
+    return totalSum;
+};
