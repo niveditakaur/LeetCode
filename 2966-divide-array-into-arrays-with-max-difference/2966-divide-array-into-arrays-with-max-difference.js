7cacda7c7d8167d360940973fab896a1a698b0db
Nivedita Kaur <niveditakaur5244@gmail.com>
2024-02-04 21:41:15 +0530

    Time: 370 ms (29.71%), Space: 82.7 MB (27.78%) - LeetHub

---
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[][]}
+ */
+var divideArray = function(nums, k) {
+    const ans=[];
+    nums.sort((a, b) => a - b);
+    for(let i=0;i<nums.length;i+=3){
+        if(nums[i+2]-nums[i]<=k){
+            ans.push([nums[i],nums[i+1],nums[i+2]]);
+        }
+        else{
+            return []
+        }
+    }
+    return ans;
+};
