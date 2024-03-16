9999bc06f66431ef97703bedcfc0bd46c563e58b
Nivedita Kaur <niveditakaur5244@gmail.com>
2024-03-16 21:45:14 +0530

    Time: 86 ms (74.03%), Space: 56.6 MB (77.92%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxLength = function(nums) {
+    const map = new Map(); 
+    map.set(0, -1); 
+    let maxLen = 0;
+    let count = 0; 
+
+    for (let i = 0; i < nums.length; i++) {
+        count += nums[i] === 0 ? -1 : 1;
+
+        if (map.has(count)) {
+            maxLen = Math.max(maxLen, i - map.get(count));
+        } else {
+            map.set(count, i);
+        }
+    }
+
+    return maxLen;
+};
